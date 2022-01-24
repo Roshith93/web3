c630972894220791464595a8b5e52ee8f8ac466b
Roshith R <r.roshith@gmail.com>
2022-01-24 17:10:49 +0530

    Set up the components

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
+import { Footer, Loader, Navbar, Transactions, Welcome , Services} from './components'
 const App = () => {
   return (
-    <div className='App'>
+    <div className='min-h-screen'>
       <h1> Hello From Web 3</h1>
+      <div className="gradient-bg-welcome">
+        <Navbar />
+        <Welcome />
+      </div>
+      <Services />
+      <Transactions />
+      <Footer />
     </div>
   )
 }
